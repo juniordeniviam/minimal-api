b3e55ebf5f87f5e67eded855a6603de32b2598c5
José Carlos <suportejr@outlook.com>
2024-10-02 18:58:15 -0300

    Criando GET, PUT, Delete para Veiculo

--- Program.cs
+++ Program.cs
@@ -55,11 +55,36 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
 }).WithTags("Veiculos");
 
 app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) => {
-    
     var veiculos= veiculoServico.Todos(pagina);
-
     return Results.Ok(veiculos);
+}).WithTags("Veiculos");
 
+app.MapGet("/veiculos{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => {
+    var veiculo= veiculoServico.BuscaPorId(id);
+    if(veiculo == null) return Results.NotFound();
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
+app.MapPut("/veiculos{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => {
+    var veiculo= veiculoServico.BuscaPorId(id);
+    if(veiculo == null) return Results.NotFound();
+
+    veiculo.Nome = veiculoDTO.Nome;
+    veiculo.Marca = veiculoDTO.Marca;
+    veiculo.Ano = veiculoDTO.Ano;
+
+    veiculoServico.Atualizar(veiculo);
+
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
+app.MapDelete("/veiculos{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => {
+    var veiculo= veiculoServico.BuscaPorId(id);
+    if(veiculo == null) return Results.NotFound();
+
+    veiculoServico.Apagar(veiculo);
+
+    return Results.NoContent();
 }).WithTags("Veiculos");
 #endregion
 
