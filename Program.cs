02d638680a9147dc70f76947766c4a0a114399f1
José Carlos <suportejr@outlook.com>
2024-10-02 19:22:40 -0300

    Criando validação ao cadastrar e atualizar veículo

--- Program.cs
+++ Program.cs
@@ -41,8 +41,30 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
 #endregion
 
 #region Veiculos
-app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico)=>{
+ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
+{
+    var validacao = new ErrosDeValidacao{
+        Mensagens = new List<string>()
+    };
     
+    if(string.IsNullOrEmpty(veiculoDTO.Nome))
+        validacao.Mensagens.Add("O campo nome é obrigatório");
+
+    if(string.IsNullOrEmpty(veiculoDTO.Marca))
+        validacao.Mensagens.Add("O campo Marca é obrigatório");    
+    
+    if(veiculoDTO.Ano < 1950)
+        validacao.Mensagens.Add("Veículo muito antigo, Somente é aceito anos superiores a 1950");
+
+    return validacao;
+}
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico)=>{  
+
+    var validacao = validaDTO(veiculoDTO);
+    if(validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);  
+
     var veiculo = new Veiculo{
         Nome = veiculoDTO.Nome,
         Marca = veiculoDTO.Marca,
@@ -66,8 +88,13 @@ app.MapGet("/veiculos{id}", ([FromRoute] int id, IVeiculoServico veiculoServico)
 }).WithTags("Veiculos");
 
 app.MapPut("/veiculos{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => {
+
     var veiculo= veiculoServico.BuscaPorId(id);
     if(veiculo == null) return Results.NotFound();
+
+    var validacao = validaDTO(veiculoDTO);
+    if(validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
 
     veiculo.Nome = veiculoDTO.Nome;
     veiculo.Marca = veiculoDTO.Marca;
